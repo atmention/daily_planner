d43c57ef6412606c62c56a1a254d557d06401808
Bemourn <70115233+koenib@users.noreply.github.com>
2021-11-16 12:23:47 -0700

    additional pseudo code and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,38 +3,56 @@
 
 // init global variables
 moment().format();
-
+// store the hours in the day
+let dailyHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 // bonus: allow days to be set as 8am, 9am, ending at diff times, etc.
 
-// currentDay is retrieved from moment.js
+// current hour is retrieved from moment.js
+let currentHour = parseInt(moment().hour());
 
-// when the page loads, display the current page
+
+// when the page loads, 
+
+// if there is no stored data for a user,
+
+// then load the default times of 9AM - 5PM
+
+
+
 // and load any user's data from storage
-// display the default layout for daily planner (8 hours)
+// TODO: things that need to be stored for the user
+    // time offset (default: 9am)
+    // array of 9 cards, each with:
+        // text content
+        // bonus: color
 
 
 // color code the current, past, and future cards
+// TODO: add data-hour attribute to cards and implement dynamic styling
+// check the user's starting hour for the day
+let startHour = parseInt((moment().hour(dailyHours[0]).hour()));
 
-let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]; // hours in the day
-let currenthour = parseInt(moment().hour());
-let starthour = parseInt((moment().hour(hours[0]).hour()));
-
-console.log(`Current hour is: ${currenthour}`);
-console.log(`Starting time of the loop is: ${currenthour}`);
+console.log(`Current hour is: ${currentHour}`);
+console.log(`Starting hour of the loop is: ${dailyHours[0]}`);
 console.log(` `);
 
-for (let i = 0; i < hours.length; i++) {
+for (let i = 0; i < dailyHours.length; i++) {
 
     // check if hour is past
-    if (parseInt((moment().hour(hours[i]).hour())) < currenthour) {
-        console.log(`The hour ${hours[i]} will be grey`);
+    if (parseInt((moment().hour(dailyHours[i] ).hour())) < currentHour) {
+        // set the card style to greyed-out
+        // TODO: the card with data-hour attribute === dailyHours[i] is greyed out
+        console.log(`Cards with the hour ${dailyHours[i]} will be grey`);
 
     // check if hour is current
-    } else if (parseInt((moment().hour(hours[i]).hour())) === currenthour) {
-        console.log(`The hour ${hours[i]} will be highlighted`);
+    } else if (parseInt((moment().hour(dailyHours[i]).hour())) === currentHour) {
+        // set the card style to highlighted
+        // TODO: the card with data-hour attribute === dailyHours[i] is highlighted
+        console.log(`Cards with the hour ${dailyHours[i]} will be highlighted`);
 
     } else {
-        console.log("This is a future time");
+        // do nothing to these cards
+        console.log(`Cards with the hour ${dailyHours[i]} will be for a future time`);
     }
 }
 
@@ -49,13 +67,13 @@ for (let i = 0; i < hours.length; i++) {
 // bonus: warn users before leaving that there are unsaved changes
 
 // listen for a user clicking on the lock
+// then save the text and color value to localStorage
 
 
 // allow buttons to change color of text boxes
 
 // listen for user clicks on buttons
-// change the background color to the selected color
-// store this and prepare to save
-
+// then change the background color to the selected color
+// bonus: possibly change styles/text color if appropriate
 
 // TODO: Research how to play haptic sounds when hovering, etc.
