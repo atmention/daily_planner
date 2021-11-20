741b71234c6950b3d752cefb7e4c19b4aa27a591
charbroiled <70115233+koenib@users.noreply.github.com>
2021-11-20 10:38:49 -0700

    localStorage and default settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,27 +17,46 @@ let workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 // bonus: allow days to be set as 8am, 9am, ending at diff times, etc.
 
 // get any local user configs
-const userSettings = localStorage.getItem("userSettings");
-console.log(userSettings);
+let userSettings = JSON.parse(localStorage.getItem("userSettings") || "[]");
+console.log("Number of settings saved: " + userSettings.length);
 
 // if there is no stored data for a user,
-if (userSettings === null) {
+if (userSettings.length === 0) {
     // then load the default times of 9AM - 5PM
     // TODO: Create the default array - With loop..?
 
     // array of objects, each object containing the text
     // and configuration for each card on the page.
+    console.log("There are no user settings saved.");
+    console.log("Initializing default settings...");
+
+    let settingsArray = [];
+    let apm = '';
+    for (let i = 0; i < 9; i++) {
+        if (workingHours[i] < 12) {
+            apm = 'am';
+        } else {
+            apm = 'pm';
+        }
+        settingsArray[i] = {id: i, hour: workingHours[i], apm: apm, text: '', color: 'gray',};
+    }
+
     // userSettings = [{
     //     id: 0,
     //     hour: 9,
-    //     apm: am,
+    //     apm: 'am',
     //     text: '',
     //     color: 'gray',
     // }];
-    console.log("There is no user data to be loaded.");
+
+    localStorage.setItem("userSettings", JSON.stringify(settingsArray));
+    userSettings = JSON.parse(localStorage.getItem("userSettings"));
+    console.log("New settings saved successfully.")
 } else
 
-console.log("There is user data to be loaded.");
+console.log("There are custom user settings to load.");
+console.log("Displaying current user settings...");
+console.log(userSettings);
 // load any user's data from storage
 // TODO: things that need to be stored for the user
     // time offset (default: 9am)
@@ -45,8 +64,9 @@ console.log("There is user data to be loaded.");
         // text content
         // bonus: color
 
+
 // function to loop through cards and update the timezone
-const refreshCardTimezone = function(startTime) {
+function refreshCardTimezone(startTime) {
     // for userSettings.length
     // set hour, am/pm, etc., to new values
 }
@@ -68,6 +88,7 @@ const refreshCardTimezone = function(startTime) {
 //         // set the card style to greyed-out
 //         // TODO: the card with data-hour attribute === dailyHours[i] is greyed out
 //         console.log(`Cards with the hour ${dailyHours[i]} will be grey`);
+//         // 
 
 //     // check if hour is current
 //     } else if (parseInt((moment().hour(dailyHours[i]).hour())) === currentHour) {
@@ -80,13 +101,6 @@ const refreshCardTimezone = function(startTime) {
 //         console.log(`Cards with the hour ${dailyHours[i]} will be for a future time`);
 //     }
 // }
-
-
-// allow the text boxes to have text entered into them
-
-// listen for a user clicking inside of a text boxes
-// then open the text box for editing
-
 
 // add save functionality to text/card
 // bonus: warn users before leaving that there are unsaved changes
